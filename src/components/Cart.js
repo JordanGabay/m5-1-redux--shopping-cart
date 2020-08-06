c803164f6086e2d7aff9d746505905c870ff1231
Jordan Gabay <gabay.jordan@outlook.com>
2020-08-06 17:31:18 -0400

    Exercise 3b Complete

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,26 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import { MdRemoveCircleOutline } from "react-icons/md";
+import { useSelector } from "react-redux";
+import {getStoreItemArray} from '../reducers';
+import CartItem from './CartItem';
 
 const Cart = () => {
+  const state = useSelector((state) => state);
+  const storeItems = useSelector(getStoreItemArray);
   return (
     <Sidebar>
       <h2>Your Cart</h2>
       <span>1 item</span>
-      <ShoppingCart>
-        <li>
-          <div>
-            <h3>Hello World</h3>
-            <Remove>
-              <MdRemoveCircleOutline />
-            </Remove>
-          </div>
-          <div>
-            <label>Quantity</label>
-            <Quantity type="number" />
-          </div>
-        </li>
-      </ShoppingCart>
+    {storeItems.map((item) => {
+        return <CartItem item={item}/>
+    })}
     </Sidebar>
   );
 };
@@ -34,43 +27,5 @@ const Sidebar = styled.div`
   padding: 2rem;
 `;
 
-const ShoppingCart = styled.ul`
-  padding: 0;
-  li {
-    list-style: none;
-    background: transparent;
-    border: dashed grey;
-  }
-  li h3 {
-    padding-left: 0.5rem;
-  }
-  li div:first-child {
-    display: flex;
-    justify-content: space-between;
-    padding: 0;
-  }
-  li div:last-child {
-    display: flex;
-    background-color: #431c53;
-    height: 60px;
-    align-items: center;
-    padding-left: 0.5rem;
-  }
-  li label {
-    margin-right: 0.5rem;
-  }
-`;
-const Quantity = styled.input`
-  width: 50px;
-  height: 30px;
-`;
 
-const Remove = styled.button`
-  background: transparent;
-  color: white;
-  padding: 0.5rem;
-  font-size: 1.25rem;
-  border: none;
-  margin-right: 1rem;
-`;
 export default Cart;
